bd8b5effd825df31721e2347d75a79c7d00a774b
Masato Oikawa <masato.oikawa@upstairs.jp>
2026-04-09 11:48:58 +0900

    Add no relation coverity issue

--- ant/src/oata/HelloWorld.java
+++ ant/src/oata/HelloWorld.java
@@ -1,6 +1,11 @@
 package oata;
 
 public class HelloWorld {
+    public static int selfContainedNullDeref() {
+        String local = null;
+        return local.length();
+    }
+
     public static void main(String[] args) {
         String s = null;
         CrossRepoSink.consume(s);
