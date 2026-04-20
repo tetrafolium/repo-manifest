d7855abd9c4b76222a64e4a3b496346161dec388
Masato Oikawa <masato.oikawa@upstairs.jp>
2026-04-20 15:42:48 +0900

    use dummy code

--- ant/src/oata/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package oata;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        String s = null;
-        CrossRepoSink.consume(s);
-        System.out.println("Hello World");
-    }
-}
