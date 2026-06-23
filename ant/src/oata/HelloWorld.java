1c0c8b4325202e4668f0b5d1a201626fb63f461e
TE-ShomeiRyo <shomei.ryo@sony.com>
2026-06-23 13:12:53 +0900

    Delete ant directory
    Signed-off-by: TE-ShomeiRyo <shomei.ryo@sony.com>

--- ant/src/oata/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package oata;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        String s = null;
-	s.length();
-        System.out.println("Hello World");
-    }
-}
